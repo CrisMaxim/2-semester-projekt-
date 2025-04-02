a281c74bbca748a680f3632bcc60850402f78854
Martin55530 <55530@student.eadania.dk>
2025-04-02 13:24:31 +0200

    Update 2 sem script.R
    Prøver at uploade fra dev_martin til main

--- 2 sem script.R
+++ 2 sem script.R
@@ -1,8 +1,13 @@
 # Commit nasbiuFKJSDB
 # HEJ GRUPPE
 #hej igen
+<<<<<<< Updated upstream
 
 # Hej fra Martin
 
 # Godt
 # JA DA 
+=======
+# Hej fra Martin
+# Test dev_Martin
+>>>>>>> Stashed changes
